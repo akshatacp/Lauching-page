9133c63c70a3a713f808f6037d8f825c2c2115af
Akshata patil <113229445+akshatacp@users.noreply.github.com>
2024-05-28 20:03:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('learn-more-btn').addEventListener('click', function() {
+    window.location.href = '#about';
+});
+
+document.getElementById('enroll-btn').addEventListener('click', function() {
+    alert('Thank you for your interest! Enrollment details will be sent to your email.');
+});
